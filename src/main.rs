ca44361448c6ec7aacbde2c3932ff510ef12e9b4
LucasOe <github@lucasoe.com>
2022-08-21 15:42:35 +0200

    Multiple writes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
-use serde_json::json;
-use std::io::Write;
-use std::net::TcpStream;
+use serde_json::{json, Value};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // first
-    let stream = TcpStream::connect("127.0.0.1:39999").unwrap();
-    write(
-        &stream,
+    let data = send(
         json!({
             "messageID": 3,
             "guid":"-1",
             "script":"print(\"Hello, World\")"
         })
         .to_string(),
-    );
+    )
+    .unwrap();
+    let result: Value = serde_json::from_str(&data).unwrap();
+    println!("{:?}", result);
 
     // second
-    let new_stream = TcpStream::connect("127.0.0.1:39999").unwrap();
-    write(
-        &new_stream,
+    let data = send(
         json!({
             "messageID": 3,
             "guid":"-1",
             "script":"print(\"Hello, Mars\")"
         })
         .to_string(),
-    );
+    )
+    .unwrap();
+    let result: Value = serde_json::from_str(&data).unwrap();
+    println!("{:?}", result);
 }
 
-fn write(mut stream: &TcpStream, msg: String) {
+fn send(msg: String) -> Option<String> {
+    let mut stream = TcpStream::connect("127.0.0.1:39999").unwrap();
     stream.write_all(msg.as_bytes()).unwrap();
     stream.flush().unwrap();
+
+    let listener = TcpListener::bind("127.0.0.1:39998").unwrap();
+    match listener.accept() {
+        Ok((stream, _addr)) => Some(read(&stream)),
+        Err(_) => None,
+    }
+}
+
+fn read(mut stream: &TcpStream) -> String {
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer).unwrap();
+    stream.flush().unwrap();
+    buffer
 }
