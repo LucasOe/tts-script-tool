fefc65927fa6c73ee2b2abec355ff0c15b680d26
LucasOe <github@lucasoe.com>
2022-08-21 04:37:56 +0200

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
+use serde_json::json;
 use std::io::Write;
 use std::net::TcpStream;
+use std::{env, fs};
 
-#[allow(unused_variables)]
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
@@ -10,11 +10,12 @@ fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:39999").expect("Couldn't connect to server.");
     println!("Connected!");
 
-    let msg = br#"
-        {
-            "messageID": 3,
-            "guid": "-1",
-            "script": "print(\"Hello, World\")"
-        }"#;
-    stream.write(msg).unwrap();
+    let contents = fs::read_to_string(path).expect("Can't read file.");
+    let msg = json!({
+        "messageID": 3,
+        "guid": "-1",
+        "script": format!("{}", contents)
+    })
+    .to_string();
+    stream.write(msg.as_bytes()).unwrap();
 }
