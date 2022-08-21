77eb28cd2edcd667836f52d0f89361ee716f8ba1
LucasOe <lucas.oelker@gmx.de>
2022-08-21 04:00:24 +0200

    Execute code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::env;
+use std::io::Write;
 use std::net::TcpStream;
 
 #[allow(unused_variables)]
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
-    connect();
-}
 
-#[allow(unused_variables)]
-fn connect() {
-    match TcpStream::connect("127.0.0.1:39999") {
-        Ok(stream) => {
-            println!("Connected!");
-        }
-        Err(e) => {
-            println!("Can't connect: {}", e);
-        }
-    };
+    let mut stream = TcpStream::connect("127.0.0.1:39999").expect("Couldn't connect to server.");
+    println!("Connected!");
+
+    let msg = br#"
+        {
+            "messageID": 3,
+            "guid": "-1",
+            "script": "print(\"Hello, World\")"
+        }"#;
+    stream.write(msg).unwrap();
 }
