88828b71441ae325f6b4586333cbfe7db4dece93
LucasOe <lucas.oelker@gmx.de>
2022-08-21 15:31:32 +0200

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,34 @@
-use serde_json::{json, Value};
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::{env, fs};
+use serde_json::json;
+use std::io::Write;
+use std::net::TcpStream;
 
-#[allow(unused_variables)]
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
+    // first
+    let stream = TcpStream::connect("127.0.0.1:39999").unwrap();
+    write(
+        &stream,
+        json!({
+            "messageID": 3,
+            "guid":"-1",
+            "script":"print(\"Hello, World\")"
+        })
+        .to_string(),
+    );
 
-    let stream = TcpStream::connect("127.0.0.1:39999").expect("Couldn't connect to server.");
-    println!("Connected!");
-
-    // get objects
-    let msg = json!({
-        "messageID": 0
-    })
-    .to_string();
-    write(&stream, msg);
-
-    let data = listen().unwrap();
-    let result: Value = serde_json::from_str(&data).unwrap();
-    println!("{:?}", result);
-
-    // execute script
-    let new_stream = TcpStream::connect("127.0.0.1:39999").expect("Couldn't connect to server.");
-    let msg = json!({
-        "messageID": 3,
-        "guid": "-1",
-        "script": "print(\"Hello, World!\")"
-    })
-    .to_string();
-    write(&new_stream, msg);
-
-    let data = listen().unwrap();
-    let result: Value = serde_json::from_str(&data).unwrap();
-    println!("{:?}", result);
-
-    let contents = fs::read_to_string(path).expect("Can't read file.");
-}
-
-fn listen() -> Option<String> {
-    let listener = TcpListener::bind("127.0.0.1:39998").unwrap();
-    match listener.accept() {
-        Ok((stream, _addr)) => Some(read(&stream)),
-        Err(_) => None,
-    }
+    // second
+    let new_stream = TcpStream::connect("127.0.0.1:39999").unwrap();
+    write(
+        &new_stream,
+        json!({
+            "messageID": 3,
+            "guid":"-1",
+            "script":"print(\"Hello, Mars\")"
+        })
+        .to_string(),
+    );
 }
 
 fn write(mut stream: &TcpStream, msg: String) {
     stream.write_all(msg.as_bytes()).unwrap();
     stream.flush().unwrap();
-}
-
-fn read(mut stream: &TcpStream) -> String {
-    let mut buffer = String::new();
-    stream.read_to_string(&mut buffer).unwrap();
-    stream.flush().unwrap();
-    buffer
 }
