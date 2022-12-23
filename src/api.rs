261e8edb37822d56d9cebcb8fb99057d8210f89c
LucasOe <lucas.oelker@gmx.de>
2022-12-23 22:34:31 +0100

    Use generics for message_id

--- src/api.rs
+++ src/api.rs
@@ -29,6 +29,22 @@ pub struct AnswerReturn {
     pub return_value: Option<String>,
 }
 
+pub trait MessageId {
+    fn get_message_id() -> u64;
+}
+
+impl MessageId for AnswerReload {
+    fn get_message_id() -> u64 {
+        1
+    }
+}
+
+impl MessageId for AnswerReturn {
+    fn get_message_id() -> u64 {
+        5
+    }
+}
+
 impl AnswerReload {
     pub fn get_script_states(&self) -> Result<Value> {
         let script_states = &self.script_states;
@@ -44,6 +60,28 @@ impl AnswerReturn {
             .context("returnValue doesn't exist")?;
         Ok(serde_json::from_str(&return_value)?)
     }
+}
+
+/// Get lua scripts
+pub fn message_get_lua_scripts() -> Result<AnswerReload> {
+    send(
+        json!({
+            "messageID": 0,
+        })
+        .to_string(),
+    )
+}
+
+/// Update the lua scripts and UI XML for any objects listed in the message,
+/// and then reload the save file. Objects not mentioned are not updated.
+pub fn message_reload(script_states: Value) -> Result<AnswerReload> {
+    send(
+        json!({
+            "messageID": 1,
+            "scriptStates": script_states
+        })
+        .to_string(),
+    )
 }
 
 /// Executes lua code inside Tabletop Simulator and returns the value.
@@ -68,30 +106,5 @@ pub fn message_execute(script: String) -> Result<AnswerReturn> {
             "script": script
         })
         .to_string(),
-        5,
-    )
-}
-
-/// Update the lua scripts and UI XML for any objects listed in the message,
-/// and then reload the save file. Objects not mentioned are not updated.
-pub fn message_reload(script_states: Value) -> Result<AnswerReload> {
-    send(
-        json!({
-            "messageID": 1,
-            "scriptStates": script_states
-        })
-        .to_string(),
-        1,
-    )
-}
-
-/// Get lua scripts
-pub fn message_get_lua_scripts() -> Result<AnswerReload> {
-    send(
-        json!({
-            "messageID": 0,
-        })
-        .to_string(),
-        1,
     )
 }
