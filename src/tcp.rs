a23e4d0d744d5ac0298707fa4ad8e5ff96f48fa1
LucasOe <lucas.oelker@gmx.de>
2023-01-05 22:52:07 +0100

    Add answer_any function

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,11 +1,17 @@
-use crate::api::{Answer, AnswerError, Message};
-use anyhow::{anyhow, Result};
+use crate::api::{Answer, AnswerError};
+use crate::{
+    AnswerCustomMessage, AnswerGameSaved, AnswerNewObject, AnswerObjectCreated, AnswerPrint,
+    AnswerReload, AnswerReturn, HasId,
+};
+use anyhow::{anyhow, bail, Result};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use serde_json::Value;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 /// Sends a message to Tabletop Simulator and returns the answer as a Value::Object.
-pub fn send<T: Message>(message: &T) -> Result<()> {
+pub fn send<T: Serialize>(message: &T) -> Result<()> {
     let msg = serde_json::to_string(&message)?;
     let mut stream = TcpStream::connect("127.0.0.1:39999")?;
     stream.write_all(msg.as_bytes()).unwrap();
@@ -15,7 +21,7 @@ pub fn send<T: Message>(message: &T) -> Result<()> {
 }
 
 /// Waits for an answer with the correct id. Returns an Error if `AnswerError` is revieved.
-pub fn read<T: Answer>() -> Result<T> {
+pub fn read<T: HasId + DeserializeOwned>() -> Result<T> {
     let listener = TcpListener::bind("127.0.0.1:39998")?;
     let answer: Value = loop {
         let (mut stream, _addr) = listener.accept()?;
@@ -33,6 +39,29 @@ pub fn read<T: Answer>() -> Result<T> {
     }?;
 
     Ok(serde_json::from_value(answer)?)
+}
+
+#[rustfmt::skip]
+pub fn read_any() -> Result<Box<dyn Answer>> {
+    let listener = TcpListener::bind("127.0.0.1:39998")?;
+    let (mut stream, _addr) = listener.accept()?;
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer).unwrap();
+
+    let message: Value = serde_json::from_str(&buffer)?;
+    let message_id = get_message_id(&message);
+
+    match message_id {
+        AnswerNewObject::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerNewObject>(message)?)),
+        AnswerReload::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerReload>(message)?)),
+        AnswerPrint::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerPrint>(message)?)),
+        AnswerError::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerError>(message)?)),
+        AnswerCustomMessage::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerCustomMessage>(message)?)),
+        AnswerReturn::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerReturn>(message)?)),
+        AnswerGameSaved::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerGameSaved>(message)?)),
+        AnswerObjectCreated::MESSAGE_ID => Ok(Box::new(serde_json::from_value::<AnswerObjectCreated>(message)?)),
+        _ => bail!("Can't find id")
+    }
 }
 
 fn get_message_id(message: &Value) -> u8 {
