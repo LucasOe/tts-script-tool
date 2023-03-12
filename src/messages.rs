6acb3693b255652fd23765b77ea3f8e5fed50559
LucasOe <lucas.oelker@gmx.de>
2023-03-12 14:15:55 +0100

    Move all messages to messages.rs

--- src/messages.rs
+++ src/messages.rs
@@ -3,18 +3,7 @@ use crate::execute;
 use std::collections::HashMap;
 use tts_external_api::ExternalEditorApi;
 
-/// Sets the script for the object.
-pub fn set_script(api: &ExternalEditorApi, guid: &str, script: &str) -> Result<()> {
-    execute!(
-        api,
-        r#"
-            getObjectFromGUID("{guid}").setLuaScript("{}")
-        "#,
-        script.escape_default()
-    )
-}
-
-/// Returns a list of all guids
+/// Returns a list of all object guids in the current save
 pub fn get_objects(api: &ExternalEditorApi) -> Result<Vec<String>> {
     execute!(
         api,
@@ -28,8 +17,8 @@ pub fn get_objects(api: &ExternalEditorApi) -> Result<Vec<String>> {
     )
 }
 
-/// Returns a list of tags associated with each object
-pub fn get_tags(api: &ExternalEditorApi) -> Result<HashMap<String, Vec<String>>> {
+/// Returns a list of tags associated with each object in the current save
+pub fn get_tag_map(api: &ExternalEditorApi) -> Result<HashMap<String, Vec<String>>> {
     execute!(
         api,
         r#"
@@ -41,5 +30,39 @@ pub fn get_tags(api: &ExternalEditorApi) -> Result<HashMap<String, Vec<String>>>
             end
             return JSON.encode(list)
         "#,
+    )
+}
+
+/// Returns a list of tags for an object
+pub fn get_tags(api: &ExternalEditorApi, guid: &str) -> Result<Vec<String>> {
+    execute!(
+        api,
+        r#"
+            return JSON.encode(getObjectFromGUID("{guid}").getTags())
+        "#,
+    )
+}
+
+/// Set the script for an object
+pub fn set_script(api: &ExternalEditorApi, guid: &str, script: &str) -> Result<()> {
+    let escaped_script = script.escape_default();
+    execute!(
+        api,
+        r#"
+            getObjectFromGUID("{guid}").setLuaScript("{escaped_script}")
+        "#
+    )
+}
+
+/// Adds a list of tags to an object
+pub fn add_tags(api: &ExternalEditorApi, guid: &str, tags: &Vec<String>) -> Result<()> {
+    let tags = serde_json::to_string(tags)?;
+    let escaped_tags = tags.escape_default();
+    execute!(
+        api,
+        r#"
+            tags = JSON.decode("{escaped_tags}")
+            getObjectFromGUID("{guid}").setTags(tags)
+        "#
     )
 }
