82f7b2c8857d57fdd1b2e68cc8efd8639b4a0c85
LucasOe <github@lucasoe.com>
2023-03-12 17:47:15 +0100

    Refactor functions

--- src/app.rs
+++ src/app.rs
@@ -13,18 +13,18 @@ use tts_external_api::{json, ExternalEditorApi};
 pub fn attach(api: &ExternalEditorApi, path: &PathBuf, guid: Option<String>) -> Result<()> {
     let path = Path::new(path);
     let file_name = path.file_name().unwrap().to_str().unwrap();
-    let guid = match guid {
-        Some(guid) => guid,
-        None => select_object(api)?,
-    };
-    guid_exists(api, &guid)?;
+
+    let guid = get_guid(api, guid)?;
     let tag = set_tag(api, file_name, &guid)?;
     print_info!("added:", "'{tag}' as a tag for '{guid}'");
+
     let file_content = fs::read_to_string(path)?;
     set_script(api, &guid, &file_content)?;
     print_info!("updated:", "'{guid}' with tag '{tag}'");
+
     api.reload(json!([]))?;
     set_tag(api, file_name, &guid)?;
+
     print_info!("reloaded save!");
     println!("To save the applied tag it is recommended to save the game before reloading.");
     Ok(())
@@ -36,7 +36,7 @@ pub fn reload(api: &ExternalEditorApi, path: &PathBuf) -> Result<()> {
 
     // update scripts with setLuaScript(), so objects without a script get updated.
     for (guid, tags) in guid_tags {
-        let (tags, _) = get_valid_tags(tags);
+        let (tags, _) = partition_tags(tags);
         // ensure that the object only has one valid tag
         let valid_tag: Option<String> = match tags.len() {
             1 => Some(tags[0].clone()),
@@ -67,14 +67,13 @@ pub fn reload(api: &ExternalEditorApi, path: &PathBuf) -> Result<()> {
     // get global ui from the save data
     let global_ui = unescape(&script_states[0].get("ui").unwrap().to_string()).unwrap();
 
-    let message = json!([{
+    api.reload(json!([{
         "guid": "-1",
         "script": global_script,
         "ui": global_ui
-    }]);
-    api.reload(message)?;
-    print_info!("reloaded save!");
+    }]))?;
 
+    print_info!("reloaded save!");
     Ok(())
 }
 
@@ -93,13 +92,6 @@ pub fn backup(api: &ExternalEditorApi, path: &PathBuf) -> Result<()> {
     Ok(())
 }
 
-/// Shows the user a list of all objects in the save to select from.
-fn select_object(api: &ExternalEditorApi) -> Result<String> {
-    let objects = get_objects(api)?;
-    let selection = Select::new("Select the object to attach the script to:", objects).prompt()?;
-    Ok(selection)
-}
-
 /// Add the file as a tag. Tags use "scripts/<File>.ttslua" as a naming convention.
 // Guid has to be global so objects without scripts can execute code.
 fn set_tag(api: &ExternalEditorApi, file_name: &str, guid: &str) -> Result<String> {
@@ -108,16 +100,16 @@ fn set_tag(api: &ExternalEditorApi, file_name: &str, guid: &str) -> Result<Strin
     let tags = get_tags(api, guid)?;
 
     // set new tags for object
-    let (_, mut tags) = get_valid_tags(tags);
+    let (_, mut tags) = partition_tags(tags);
     tags.push(String::from(&tag));
     add_tags(api, guid, &tags)?;
 
     Ok(tag)
 }
 
-/// Split the tags into valid and non valid tags
-// Get the tags that follow the "scripts/<File>.ttslua" naming convention.
-fn get_valid_tags(tags: Vec<String>) -> (Vec<String>, Vec<String>) {
+/// Split the tags into valid and invalid tags.
+/// Tags that follow the "scripts/<File>.ttslua" naming convention are valid.
+fn partition_tags(tags: Vec<String>) -> (Vec<String>, Vec<String>) {
     let exprs = Regex::new(r"^(scripts/)[\d\w]+(\.lua|\.ttslua)$").unwrap();
     tags.into_iter().partition(|tag| exprs.is_match(tag))
 }
@@ -130,10 +122,27 @@ fn get_file_from_tag(path: &PathBuf, tag: &str) -> String {
     String::from(file_path.to_string_lossy())
 }
 
-/// Returns an Error if the guid doesn't exist in the current save
-fn guid_exists(api: &ExternalEditorApi, guid: &String) -> Result<()> {
-    get_objects(api)?
-        .contains(guid)
-        .then_some(())
-        .ok_or(Error::MissingGuid(guid.to_owned()))
+/// If no guid is provided show a selection of objects in the current save.
+/// Otherwise ensure that the guid provided exists. Returns [`Error::MissingGuid`] if it does not exist.
+fn get_guid(api: &ExternalEditorApi, guid: Option<String>) -> Result<String> {
+    match guid {
+        Some(guid) => guid_exists(api, guid),
+        None => select_object(api),
+    }
+}
+
+/// Returns [`Error::MissingGuid`] if the guid doesn't exist in the current save
+fn guid_exists(api: &ExternalEditorApi, guid: String) -> Result<String> {
+    match get_objects(api)?.contains(&guid) {
+        true => Ok(guid),
+        false => Err(Error::MissingGuid(guid)),
+    }
+}
+
+/// Shows the user a list of all objects in the save to select from
+fn select_object(api: &ExternalEditorApi) -> Result<String> {
+    let objects = get_objects(api)?;
+    Select::new("Select the object to attach the script to:", objects)
+        .prompt()
+        .map_err(Error::InquireError)
 }
