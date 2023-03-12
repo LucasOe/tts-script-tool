050bf225c62d79897e1801c0146c74fb77fc9685
LucasOe <lucas.oelker@gmx.de>
2023-03-12 17:57:52 +0100

    Move colorize::AnsiColor inside macros.rs

--- src/macros.rs
+++ src/macros.rs
@@ -2,16 +2,18 @@
 macro_rules! execute {
     ($api:ident, $($arg:tt)*) => {{
 		let result = $api.execute(format!($($arg)*))?.return_value;
-        serde_json::from_value(result).map_err(crate::error::Error::SerdeError)
+        serde_json::from_value(result).map_err($crate::error::Error::SerdeError)
     }}
 }
 
 #[macro_export]
 macro_rules! print_info {
-    ($label:expr) => {
+    ($label:expr) => {{
+        use colorize::AnsiColor;
         println!("{}", $label.green().bold());
-    };
-    ($label:expr, $($arg:tt)*) => {
+    }};
+    ($label:expr, $($arg:tt)*) => {{
+        use colorize::AnsiColor;
         println!("{} {}", $label.yellow().bold(), format!($($arg)*));
-    };
+    }};
 }
