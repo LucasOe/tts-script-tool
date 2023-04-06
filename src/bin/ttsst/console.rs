00435ec148b18ccbc4708d1205ade42f196a8b64
LucasOe <github@lucasoe.com>
2023-04-06 19:22:06 +0200

    Move ExternalEditorApi constuctor to console.rs

--- src/bin/ttsst/console.rs
+++ src/bin/ttsst/console.rs
@@ -1,13 +1,13 @@
 use colorize::AnsiColor;
 use std::io::Write;
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
 use std::thread::{self, JoinHandle};
 use tts_external_api::messages::Answer;
 use tts_external_api::ExternalEditorApi;
 use ttsst::error::Result;
 
-/// Spawns a new thread that listens to the print, log and error messages in the console
-/// All messages get forwarded to port 39997 so that they can be used again
+/// Spawns a new thread that listens to the print, log and error messages in the console.
+/// All messages get forwarded to port 39997 so that they can be used again.
 pub fn console(api: ExternalEditorApi) -> JoinHandle<Result<()>> {
     thread::spawn(move || -> Result<()> {
         loop {
@@ -34,9 +34,15 @@ pub fn console(api: ExternalEditorApi) -> JoinHandle<Result<()>> {
     })
 }
 
-/// Spawns a new thread that listens to file changes in the `watch` directory
-pub fn watch(api: ExternalEditorApi) -> JoinHandle<Result<()>> {
+/// Spawns a new thread that listens to file changes in the `watch` directory.
+/// This thread uses its own `ExternalEditorApi` listening to port 39997.
+pub fn watch() -> JoinHandle<Result<()>> {
     thread::spawn(move || -> Result<()> {
+        // Constructs a new `ExternalEditorApi` listening to port 39997
+        let api = tts_external_api::ExternalEditorApi {
+            listener: TcpListener::bind("127.0.0.1:39997")?,
+        };
+
         loop {
             thread::sleep(std::time::Duration::from_secs(5));
             api.execute(r#"print("Hello")"#.to_string())?;
