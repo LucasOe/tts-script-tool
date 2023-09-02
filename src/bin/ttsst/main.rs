abe279bc6b9ce7fd9d5a2f5b29a1ee96e7152615
LucasOe <github@lucasoe.com>
2023-09-02 13:23:02 +0200

    Add clap ArgGroup for guids

--- src/bin/ttsst/main.rs
+++ src/bin/ttsst/main.rs
@@ -3,7 +3,7 @@ mod console;
 mod macros;
 mod parser;
 
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use colorize::AnsiColor;
 use std::path::PathBuf;
 use tts_external_api::ExternalEditorApi;
@@ -11,42 +11,48 @@ use ttsst::error::Result;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
+struct Cli {
     #[command(subcommand)]
     command: Commands,
+}
+
+#[derive(Args)]
+pub struct Guids {
+    /// Optional: The guid(s) of the object(s) the script should be attached to
+    #[arg(value_parser = parser::guid)]
+    guids: Option<Vec<String>>,
+
+    /// Show HandTriggers in the list of objects, if no guids are provided
+    #[arg(short, long)]
+    all: bool,
 }
 
 #[derive(Subcommand)]
 enum Commands {
     /// Attach a script to one or more objects
     Attach {
-        /// Path to the file that should be attached
+        /// Path to the file that should be attached.
+        /// ttsst will use the current working directory as a root.
+        #[arg(value_name = "FILE")]
         #[arg(value_parser = parser::path_is_file)]
         path: PathBuf,
 
-        /// Optional: The guid(s) of the object(s) the script should be attached to
-        #[arg(value_parser = parser::guid)]
-        guids: Option<Vec<String>>,
-
-        /// Show HandTriggers in the list of objects, if no guids are provided
-        #[arg(short, long)]
-        all: bool,
+        #[command(flatten)]
+        guids: Guids,
     },
 
     /// Detach a script from one or more objects
     Detach {
-        /// Optional: The guid(s) of the object(s) the script should be detached from
-        #[arg(value_parser = parser::guid)]
-        guids: Option<Vec<String>>,
-
-        /// Show HandTriggers in the list of objects, if no guids are provided
-        #[arg(short, long)]
-        all: bool,
+        #[command(flatten)]
+        guids: Guids,
     },
 
     /// Update scripts and reload save
     Reload {
-        /// Path to the directory with all scripts
+        /// Path to the directory with all scripts.
+        ///
+        /// If the path is a single file, only objects with that file attached will get reloaded.
+        #[arg(value_name = "PATH")]
         #[arg(value_parser = parser::path_exists, default_value = ".\\")]
         path: PathBuf,
     },
@@ -54,7 +60,7 @@ enum Commands {
     /// Show print, log and error messages in the console
     Console {
         /// Optional: Directory to be watched
-        #[arg(short, long)]
+        #[arg(short, long, value_name = "PATH")]
         #[arg(value_parser = parser::path_exists)]
         watch: Option<PathBuf>,
     },
@@ -68,7 +74,7 @@ enum Commands {
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = Cli::parse();
 
     if let Err(err) = run(args) {
         eprintln!("{} {}", "error:".red().bold(), err);
@@ -76,11 +82,11 @@ fn main() {
     }
 }
 
-fn run(args: Args) -> Result<()> {
+fn run(args: Cli) -> Result<()> {
     let api = ExternalEditorApi::new();
     match args.command {
-        Commands::Attach { path, guids, all } => app::attach(&api, path, guids, all)?,
-        Commands::Detach { guids, all } => app::detach(&api, guids, all)?,
+        Commands::Attach { path, guids } => app::attach(&api, path, guids)?,
+        Commands::Detach { guids } => app::detach(&api, guids)?,
         Commands::Backup { path } => app::backup(&api, path)?,
         Commands::Console { watch } => console::start(api, watch)?,
         Commands::Reload { path } => app::reload(&api, path)?,
