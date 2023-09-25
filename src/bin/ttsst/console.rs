e0041db6f9b58bfe14d903a74ff8f72cad792ea6
LucasOe <github@lucasoe.com>
2023-09-25 14:49:47 +0200

    Terminate threads on Error

--- src/bin/ttsst/console.rs
+++ src/bin/ttsst/console.rs
@@ -1,7 +1,6 @@
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::path::PathBuf;
-use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
 use colored::*;
@@ -18,13 +17,26 @@ use crate::{app, ReloadArgs};
 
 /// Show print, log and error messages in the console.
 /// If `--watch` mode is enabled, files in that directory will we watched and reloaded on change.
-pub fn start(api: ExternalEditorApi, path: Option<Vec<PathBuf>>) -> Result<()> {
-    let console_handle = console(api, path.is_some());
-    let watch_handle = path.map(watch);
+pub fn start(api: ExternalEditorApi, paths: Option<&[PathBuf]>) -> Result<()> {
+    // Note: `std::process::exit` terminates all running threads
+    std::thread::scope(|scope| {
+        scope.spawn(move || {
+            if let Err(err) = console(api, paths.is_some()) {
+                log::error!("{}", err);
+                std::process::exit(1);
+            }
+        });
 
-    // Wait for threads to finish. Threads should only finish if they return an error.
-    console_handle.join().unwrap()?;
-    watch_handle.map(|handle| handle.join().unwrap());
+        if let Some(paths) = paths {
+            scope.spawn(move || {
+                if let Err(err) = watch(paths) {
+                    log::error!("{}", err);
+                    std::process::exit(1);
+                }
+            });
+        }
+    });
+
     Err("console loop was aborted".into())
 }
 
@@ -39,44 +51,42 @@ impl PartialEq for ComparableAnswer {
 
 /// Spawns a new thread that listens to the print, log and error messages in the console.
 /// All messages get forwarded to port 39997 so that they can be used again.
-fn console(api: ExternalEditorApi, watching: bool) -> JoinHandle<Result<()>> {
-    thread::spawn(move || -> Result<()> {
-        loop {
-            // Forward the message to the TcpStream on port 39997 if a connection exists
-            let buffer = api.read_string();
-            let message: Answer = serde_json::from_str(&buffer)?;
+fn console(api: ExternalEditorApi, watching: bool) -> Result<()> {
+    loop {
+        // Forward the message to the TcpStream on port 39997 if a connection exists
+        let buffer = api.read_string();
+        let message: Answer = serde_json::from_str(&buffer)?;
 
-            // Note: When reloading there isn't a strict order of messages sent from the server
-            match message {
-                // Only forward `Answer::AnswerReload` messages if watching
-                Answer::AnswerReload(_) if watching => {
-                    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:39997") {
-                        stream.write_all(buffer.as_bytes())?;
-                        stream.flush()?;
-                    }
-                }
-
-                // Print all messages
-                //
-                // Skips `Answer::AnswerReload` when watching, otherwise reloading
-                // would cause multiple messages to print
-                _ => {
-                    // The debouncer adds a small delay, so that log messages
-                    // are printed before in-game messages for better ordering
-                    let debouncer = EventDebouncer::new(
-                        Duration::from_millis(100),
-                        move |data: ComparableAnswer| {
-                            if let Some(msg) = data.0.message() {
-                                let time = chrono::Local::now().format("%H:%M:%S").to_string();
-                                println!("[{}] {}", time.bright_white(), msg);
-                            };
-                        },
-                    );
-                    debouncer.put(ComparableAnswer(message));
+        // Note: When reloading there isn't a strict order of messages sent from the server
+        match message {
+            // Only forward `Answer::AnswerReload` messages if watching
+            Answer::AnswerReload(_) if watching => {
+                if let Ok(mut stream) = TcpStream::connect("127.0.0.1:39997") {
+                    stream.write_all(buffer.as_bytes())?;
+                    stream.flush()?;
                 }
             }
+
+            // Print all messages
+            //
+            // Skips `Answer::AnswerReload` when watching, otherwise reloading
+            // would cause multiple messages to print
+            _ => {
+                // The debouncer adds a small delay, so that log messages
+                // are printed before in-game messages for better ordering
+                let debouncer = EventDebouncer::new(
+                    Duration::from_millis(100),
+                    move |data: ComparableAnswer| {
+                        if let Some(msg) = data.0.message() {
+                            let time = chrono::Local::now().format("%H:%M:%S").to_string();
+                            println!("[{}] {}", time.bright_white(), msg);
+                        };
+                    },
+                );
+                debouncer.put(ComparableAnswer(message));
+            }
         }
-    })
+    }
 }
 
 trait Message {
@@ -96,40 +106,38 @@ impl Message for Answer {
 
 /// Spawns a new thread that listens to file changes in the `watch` directory.
 /// This thread uses its own `ExternalEditorApi` listening to port 39997.
-fn watch(paths: Vec<PathBuf>) -> JoinHandle<Result<()>> {
-    thread::spawn(move || -> Result<()> {
-        // Constructs a new `ExternalEditorApi` listening to port 39997
-        let api = tts_external_api::ExternalEditorApi {
-            listener: TcpListener::bind("127.0.0.1:39997")?,
-        };
+fn watch(paths: &[PathBuf]) -> Result<()> {
+    // Constructs a new `ExternalEditorApi` listening to port 39997
+    let api = tts_external_api::ExternalEditorApi {
+        listener: TcpListener::bind("127.0.0.1:39997")?,
+    };
 
-        // Create notify watcher
-        let (tx, rx) = std::sync::mpsc::channel();
-        let mut watcher = debouncer::new_debouncer(Duration::from_millis(500), tx)?;
+    // Create notify watcher
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut watcher = debouncer::new_debouncer(Duration::from_millis(500), tx)?;
 
-        for path in &paths {
-            watcher.watcher().watch(path, RecursiveMode::Recursive)?;
-        }
+    for path in paths {
+        watcher.watcher().watch(path, RecursiveMode::Recursive)?;
+    }
 
-        while let Ok(result) = rx.recv() {
-            match result {
-                Ok(events) => {
-                    let paths = events
-                        .into_iter()
-                        .filter(|event| event.kind == debouncer::DebouncedEventKind::Any)
-                        .filter_map(|event| event.path.strip_current_dir().ok())
-                        .collect_vec();
+    while let Ok(result) = rx.recv() {
+        match result {
+            Ok(events) => {
+                let paths = events
+                    .iter()
+                    .filter(|event| event.kind == debouncer::DebouncedEventKind::Any)
+                    .filter_map(|event| event.path.strip_current_dir().ok())
+                    .collect_vec();
 
-                    if !paths.is_empty() {
-                        app::reload(&api, paths, ReloadArgs { guid: None })?
-                    }
+                if !paths.is_empty() {
+                    app::reload(&api, &paths, ReloadArgs { guid: None })?
                 }
-                Err(err) => error!("{}", err),
             }
+            Err(err) => error!("{}", err),
         }
+    }
 
-        Ok(())
-    })
+    Ok(())
 }
 
 trait StripCurrentDir {
--- src/bin/ttsst/main.rs
+++ src/bin/ttsst/main.rs
@@ -111,9 +111,9 @@ fn run(args: Cli) -> Result<()> {
     match args.command {
         Commands::Attach { path, guids } => app::attach(&api, path, guids)?,
         Commands::Detach { guids } => app::detach(&api, guids)?,
-        Commands::Reload { paths, args } => app::reload(&api, paths, args)?,
+        Commands::Reload { paths, args } => app::reload(&api, &paths, args)?,
         Commands::Console => console::start(api, None)?,
-        Commands::Watch { paths } => console::start(api, Some(paths))?,
+        Commands::Watch { paths } => console::start(api, Some(&paths))?,
         Commands::Backup { path } => app::backup(&api, path)?,
     }
     Ok(())
