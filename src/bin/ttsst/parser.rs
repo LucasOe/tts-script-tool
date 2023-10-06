0cd76870d11002be53a98f131d11e5a612da6b6e
LucasOe <lucas.oelker@gmx.de>
2023-10-06 12:50:08 +0200

    Add ParseError instead of bailing

--- src/bin/ttsst/parser.rs
+++ src/bin/ttsst/parser.rs
@@ -1,35 +1,47 @@
-use anyhow::{bail, Result};
 use std::{ffi::OsStr, path::PathBuf};
+use thiserror::Error;
 
-pub fn guid(s: &str) -> Result<String> {
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("not a valid GUID")]
+    InvalidGUID,
+    #[error("not a file")]
+    NotAFile,
+    #[error("does not exist")]
+    DoesNotExist,
+    #[error("not a json file")]
+    NotJsonFile,
+}
+
+pub fn guid(s: &str) -> Result<String, ParseError> {
     let len = s.len();
-    let num = s.chars().all(|c| c.is_ascii_alphanumeric());
-    match (len, num) {
+    let is_numerical = s.chars().all(|c| c.is_ascii_alphanumeric());
+    match (len, is_numerical) {
         (6, true) => Ok(s.to_string()),
-        _ => bail!("not a valid guid"),
+        _ => Err(ParseError::InvalidGUID),
     }
 }
 
-pub fn path_is_file(s: &str) -> Result<PathBuf> {
+pub fn path_is_file(s: &str) -> Result<PathBuf, ParseError> {
     let path = PathBuf::from(s);
     match path.is_file() {
         true => Ok(path),
-        false => bail!("not a file"),
+        false => Err(ParseError::NotAFile),
     }
 }
 
-pub fn path_exists(s: &str) -> Result<PathBuf> {
+pub fn path_exists(s: &str) -> Result<PathBuf, ParseError> {
     let path = PathBuf::from(s);
     match path.exists() {
         true => Ok(path),
-        false => bail!("does not exist"),
+        false => Err(ParseError::DoesNotExist),
     }
 }
 
-pub fn path_is_json(s: &str) -> Result<PathBuf> {
+pub fn path_is_json(s: &str) -> Result<PathBuf, ParseError> {
     let path = PathBuf::from(s);
     match path.extension() == Some(OsStr::new("json")) {
         true => Ok(path),
-        false => bail!("not a json file"),
+        false => Err(ParseError::NotJsonFile),
     }
 }
